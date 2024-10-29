b2a482da5b7c704a9bf10e0b97204b44df5bb4d0
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-29 17:55:34 +0900

    [feat] : security 기본 설정

---
+++ src/main/java/io/security/oauth2/springoauth2/OAuth2ClientConfig.java
@@ -0,0 +1,22 @@
+package io.security.oauth2.springoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
