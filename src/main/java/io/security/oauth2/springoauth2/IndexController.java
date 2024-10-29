ca607d49b4f3d999b106e1408224e6d35bf7283e
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-29 17:53:34 +0900

    [feat] : Oauth2 인증정보 받아오기

--- src/main/java/io/security/oauth2/springoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springoauth2/IndexController.java
@@ -1,13 +1,59 @@
 package io.security.oauth2.springoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.HashMap;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @GetMapping("/")
     public String index() {
         return "home";
+    }
+
+    @GetMapping("/user")
+    public OAuth2User user(String accessToken) {
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+        OAuth2AccessToken oAuth2AccessToken
+                = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX);
+
+        OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
+        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+        return defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+    }
+
+    @GetMapping("/oidc")
+    public OAuth2User oidc(String accessToken, String idToken) {
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+        OAuth2AccessToken oAuth2AccessToken
+                = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX);
+
+        HashMap<String, Object> idTokenClaims = new HashMap<>();
+        idTokenClaims.put(IdTokenClaimNames.ISS, "http://localhost:8080/realms/oauth2");
+        idTokenClaims.put(IdTokenClaimNames.SUB, "OIDC");
+        idTokenClaims.put("preferred_username", "user");
+
+        OidcIdToken oidcIdToken = new OidcIdToken(idToken, Instant.now(), Instant.MAX, idTokenClaims);
+
+        OidcUserRequest oidcUserRequest = new OidcUserRequest(clientRegistration, oAuth2AccessToken, oidcIdToken);
+        OidcUserService oidcUserService = new OidcUserService();
+        return oidcUserService.loadUser(oidcUserRequest);
     }
 }
--- src/main/java/io/security/oauth2/springoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springoauth2/OAuth2ClientConfig.java
@@ -1,8 +1,10 @@
 package io.security.oauth2.springoauth2;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,11 +13,8 @@ public class OAuth2ClientConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/loginPage").permitAll()
-                        .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
-
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults());
         return httpSecurity.build();
     }
 }
