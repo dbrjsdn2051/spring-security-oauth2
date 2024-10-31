0890358e25f5056c7a00973b7d1b85a3c3e75a5f
Geonu Yu <dbrjdn2051@naver.com[C>
2024-10-31 09:55:51 +0900

    [feat] : Authentication, @AuthenticationPrincipal 사용해보기

--- src/main/java/io/security/oauth2/springoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springoauth2/IndexController.java
@@ -1,5 +1,10 @@
 package io.security.oauth2.springoauth2;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,23 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "home";
+    }
+
+    @GetMapping("/user")
+    public OAuth2User user(Authentication authentication) {
+        OAuth2AuthenticationToken authentication1 = (OAuth2AuthenticationToken) authentication;
+        return authentication1.getPrincipal();
+    }
+
+    @GetMapping("/oauth2User")
+    public OAuth2User oAuth2User(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        System.out.println("oAuth2User = " + oAuth2User);
+        return oAuth2User;
+    }
+
+    @GetMapping("/oidcUser")
+    public OidcUser oidcUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        System.out.println("oidcUser = " + oidcUser);
+        return oidcUser;
     }
 }
--- src/main/java/io/security/oauth2/springoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springoauth2/OAuth2ClientConfig.java
@@ -2,21 +2,23 @@ package io.security.oauth2.springoauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .oauth2Login(Customizer.withDefaults());
+    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2.loginPage("/login")
+                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig
+                                .baseUri("/oauth2/v1/authorization"))
+                );
 
-        return httpSecurity.build();
+        return http.build();
     }
 }
